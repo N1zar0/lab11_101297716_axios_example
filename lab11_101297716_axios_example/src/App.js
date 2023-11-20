d19777557377f35a0cad6971b45b0d7dca479d3a
N1zar0 <126731855+N1zar0@users.noreply.github.com>
2023-11-19 21:33:53 -0500

    Add files via upload

---
+++ lab11_101297716_axios_example/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PersonList from './PersonList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <PersonList />
+    </div>
+  );
+}
+
+export default App;
---
+++ lab11_101297716_axios_example/src/PersonList.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class PersonList extends Component {
+  state = {
+    persons: []
+}
+
+componentDidMount() {
+    axios.get(`https://randomuser.me/api/?results=10`)
+    .then(res => {
+        console.log(res.data);
+        const persons = res.data.results;
+        this.setState({ persons });
+    })
+}
+
+render() {
+  return (
+    <div className="container mt-5">
+      <h1 style={{ textAlign: 'center', backgroundColor: '#AAFF00' }}>User List</h1>
+      <div className="list-group">
+        {this.state.persons.map(person => (
+           <div className="list-group-item" style={{ backgroundColor: '#ADD8E6', margin: 6}}>
+            <div className="row align-items-center">
+              <div className="col-md-2">
+                <img src={person.picture.large} className="rounded-circle" alt="profile" />
+              </div>
+              <div className="col-md-10">
+                <h4>{`${person.name.title} ${person.name.first} ${person.name.last}`} </h4>
+                <hr></hr>
+                <p>User Name: <b>{person.login.username}</b></p>
+                <p>Gender: {person.gender}</p>
+                <p>Time Zone Description: {`${person.location.timezone.description}`}</p>
+                <p>Address: {`${person.location.street.number} ${person.location.street.name}, ${person.location.city}, ${person.location.state}`}</p>
+                <p>Email: {person.email}</p>            
+                <p>Birth Date and Age: {`${person.dob.date} (${person.dob.age})`}</p>
+                <p>Register Date: {person.registered.date}</p>
+                <p>Phone#: {person.phone}</p>
+                <p>Cell#: {person.cell}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+    );
+  }
+}
+export default PersonList;
